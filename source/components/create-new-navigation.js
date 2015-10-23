8a1d3a2ee11ccb4e6c2ed84e1a9680995765579c
knightflight377 <emholm12@gmail.com>
2015-10-22 21:24:43 -0500

    Created the CreateNewNavigation component.

---
+++ source/components/create-new-navigation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StartingLocation from './starting-location'
+
+class CreateNewNavigation extends React.Component {
+	render() {
+		return (
+			<div>
+				<StartingLocation/>
+			</div>
+			)
+	}
+}
+
+export default CreateNewNavigation
