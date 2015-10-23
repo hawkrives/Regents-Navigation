35653693868261443b15127892f408a02a341d65
knightflight377 <emholm12@gmail.com>
2015-10-22 21:25:05 -0500

    Made the link to the Start New Navigation page work.

--- source/index.js
+++ source/index.js
@@ -3,14 +3,16 @@ import 'isomorphic-fetch'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router, {Route, IndexRoute} from 'react-router'
+import Router, {Route, IndexRoute, Link} from 'react-router'
 
 import App from './components/app'
 import MainMenu from './components/main-menu'
+import CreateNewNavigation from './components/create-new-navigation'
 
 const routes = (
 	<Route path='/' component={App}>
 		<IndexRoute component={MainMenu} />
+			<Route path='createnewnavigation' component={CreateNewNavigation} />
 	</Route>
 )
 
@@ -18,32 +20,3 @@ const routes = (
 console.log('3. 2.. 1... Blast off! 🚀')
 
 ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'))
-
-//require('babel-runtime/core-js/promise').default = require('bluebird')
-//import 'isomorphic-fetch'
-
-//import React from 'react'
-//import ReactDOM from 'react-dom'
-//import Router, {Route, IndexRoute} from 'react-router'
-
-//import App from './components/app'
-//import Button from './components/button'
-
-//let wrapComponent = function(Component, props) {
-//	return React.createClass({
-//		render: function() {
-//			return React.createElement(Component, props)
-//		},
-//	})
-//}
-
-//const routes = (
-//	<Route path= '/' component={App}>
-//		<IndexRoute handler={wrapComponent(Button, {name: 'Start'})} />
-//	</Route>
-//)
-
-//run it
-//console.log('printing!')
-
-//ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'))
