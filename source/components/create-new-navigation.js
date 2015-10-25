4eb953d54f2f6902e40232f1c46cb538e1888d08
knightflight377 <emholm12@gmail.com>
2015-10-24 22:13:54 -0500

    Added the ending location component, common destinations button, and search button to the create new navigation page.

--- source/components/create-new-navigation.js
+++ source/components/create-new-navigation.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import StartingLocation from './starting-location'
+import EndingLocation from './ending-location'
+import Button from './button'
+import styles from './create-new-navigation.less'
 
 class CreateNewNavigation extends React.Component {
 	render() {
 		return (
 			<div>
+				<div>
+				<h1 className={styles['create-new-navigation']}>Create New Navigation</h1>
+				</div>
+				<div>
 				<StartingLocation/>
+				</div>
+				<div>
+				<EndingLocation/>
+				</div>
+				<div>
+				<Button>Common Destinations</Button> <br/>
+				<Button>Search</Button> <br/>
+				</div>
 			</div>
 			)
 	}
