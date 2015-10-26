65cac8c0b2d686670ba4af1fc4596df02774c518
Kayla Bonnstetter <bonnstet@stolaf.edu>
2015-10-26 18:24:39 -0500

    Styled saved navigation table to only have borders on the inside.

--- source/components/saved-navigation.js
+++ source/components/saved-navigation.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import MainMenuButton from './main-menu-button'
 import HelpButton from './help-button'
+import styles from './saved-navigation.less'
 
 class SavedNavigation extends React.Component {
 	render() {
 		return (
 			<div>
 				<div>
-				<h1>Saved Sessions</h1>
+				<h1 className={styles['heading']}>Saved Sessions</h1>
 				</div>
 				<div>
-				<table>
+				<table className={styles['table']}>
 					<thead>
 					<tr>
-						<th>Start Point</th>
-						<th>Destination</th>
+						<th className={styles['border']}>Start Point</th>
+						<th className={styles['border']}>Destination</th>
 					</tr>
 					</thead>
 					<tbody>
 					<tr>
+						<td className={styles['border']}>Test Cell</td>
 					</tr>
 					</tbody>
 				</table>
