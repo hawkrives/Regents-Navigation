c40c4a09706ab1f9786285f67f1cfdb055a9b2f6
knightflight377 <emholm12@gmail.com>
2015-10-28 13:57:16 -0500

    Created the saved-navigation component.

--- source/components/saved-navigation.js
+++ source/components/saved-navigation.js
@@ -1,42 +1,23 @@
-import React from 'react'
-import MainMenuButton from './main-menu-button'
-import HelpButton from './help-button'
-import styles from './saved-navigation.less'
-import range from 'lodash/utility/range'
-import map from 'lodash/collection/map'
+import React, { PropTypes } from 'react'
 
 class SavedNavigation extends React.Component {
 	render() {
 		return (
-			<div>
-				<div>
-					<h1 className={styles['heading']}>Saved Sessions</h1>
-				</div>
-				<div>
-					<table className={styles['table']}>
-						<thead>
-						<tr>
-							<th className={styles['border']}>Start Point</th>
-							<th className={styles['border']}>Destination</th>
-						</tr>
-						</thead>
-						<tbody>
-							{map(range(2), i =>
-								<tr>
-									<td>Test Start {i+1}</td>
-									<td>Test Destination {i+1}</td>
-								</tr>
-							)}
-						</tbody>
-					</table>
-				</div>
-				<div>
-					<MainMenuButton/>
-					<HelpButton/>
-				</div>
-			</div>
-		)
+			<tr>
+				<td>
+				{this.props.startinglocation}
+				</td>
+				<td>
+				{this.props.destination}
+				</td>
+			</tr>
+			)
 	}
+}
+
+SavedNavigation.propTypes = {
+	destination: PropTypes.string.isRequired,
+	startinglocation: PropTypes.string.isRequired,
 }
 
 export default SavedNavigation
