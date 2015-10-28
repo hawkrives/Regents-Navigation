6187b3feb16dc1372c2ec88fe53e0c87ea193675
knightflight377 <emholm12@gmail.com>
2015-10-28 13:56:53 -0500

    Renamed saved-navigation files to saved-navigation-screen files to prevent confusion with the saved-navigation component.

---
+++ source/components/saved-navigation-screen.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import MainMenuButton from './main-menu-button'
+import HelpButton from './help-button'
+import SavedNavigation from './saved-navigation'
+import styles from './saved-navigation-screen.less'
+//import range from 'lodash/utility/range'
+//import map from 'lodash/collection/map'
+
+class SavedNavigationScreen extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<h1 className={styles['heading']}>Saved Sessions</h1>
+				</div>
+				<div>
+					<table className={styles['table']}>
+						<thead>
+						<tr>
+							<th className={styles['border']}>Start Point</th>
+							<th className={styles['border']}>Destination</th>
+						</tr>
+						</thead>
+						<tbody>
+							<SavedNavigation destination='test' startinglocation='test1'/>
+						</tbody>
+					</table>
+				</div>
+				<div>
+					<MainMenuButton/>
+					<HelpButton/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SavedNavigationScreen
