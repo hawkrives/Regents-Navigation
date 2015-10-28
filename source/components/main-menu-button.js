671bf5caff003613b0c04581db181060a4ef21ad
knightflight377 <emholm12@gmail.com>
2015-10-28 13:18:06 -0500

    Changed the icon on the main menu button.

--- source/components/main-menu-button.js
+++ source/components/main-menu-button.js
@@ -5,7 +5,7 @@ import RoundButton from './round-button'
 class MainMenuButton extends React.Component {
 	render() {
 		return (
-			<RoundButton><Link to='/'>🏠</Link></RoundButton>
+			<RoundButton><Link to='/'> H </Link></RoundButton>
 			)
 	}
 }
