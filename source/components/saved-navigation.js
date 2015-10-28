6d28cf39fa08190230e69945c045969fcb4e0dbe
Hawken Rives <hawkrives@gmail.com>
2015-10-28 09:31:01 -0500

    Re-indent some things

--- source/components/saved-navigation.js
+++ source/components/saved-navigation.js
@@ -1,42 +1,41 @@
 import React from 'react'
 import MainMenuButton from './main-menu-button'
 import HelpButton from './help-button'
-import SavedNavigationRow from './saved-navigation-row'
 import styles from './saved-navigation.less'
+import range from 'lodash/utility/range'
+import map from 'lodash/collection/map'
 
 class SavedNavigation extends React.Component {
 	render() {
 		return (
 			<div>
 				<div>
-				<h1 className={styles['heading']}>Saved Sessions</h1>
+					<h1 className={styles['heading']}>Saved Sessions</h1>
 				</div>
 				<div>
-				<table className={styles['table']}>
-					<thead>
-					<tr>
-						<th className={styles['border']}>Start Point</th>
-						<th className={styles['border']}>Destination</th>
-					</tr>
-					</thead>
-					<tbody>
-						<SavedNavigationRow>
-							<td>Test Start</td>
-							<td>Test Destination</td>
-						</SavedNavigationRow>
-						<SavedNavigationRow>
-							<td>Test Start 2</td>
-							<td>Test Destination 2</td>
-						</SavedNavigationRow> 
-					</tbody>
-				</table>
+					<table className={styles['table']}>
+						<thead>
+						<tr>
+							<th className={styles['border']}>Start Point</th>
+							<th className={styles['border']}>Destination</th>
+						</tr>
+						</thead>
+						<tbody>
+							{map(range(2), i =>
+								<tr>
+									<td>Test Start {i+1}</td>
+									<td>Test Destination {i+1}</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
 				</div>
 				<div>
-				<MainMenuButton/>
-				<HelpButton/>
+					<MainMenuButton/>
+					<HelpButton/>
 				</div>
 			</div>
-			)
+		)
 	}
 }
 
