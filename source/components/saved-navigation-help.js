d6f0f5c9864eac6ad6a97c2017cfaac8b97b7ed5
knightflight377 <emholm12@gmail.com>
2015-10-29 22:20:54 -0500

    Created the help screen for saved navigation.

---
+++ source/components/saved-navigation-help.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './saved-navigation-help.less'
+import BackButton from './back-button'
+
+class SavedNavigationHelp extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<h1 className={styles['help']}>This is where the saved navigation help text will go!</h1>
+				</div>
+				<div>
+					<BackButton link_to='/savednavigationscreen'/>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default SavedNavigationHelp
