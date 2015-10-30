e558d5daa5bae6d8e4003ccac8a476786fa55376
knightflight377 <emholm12@gmail.com>
2015-10-29 22:19:32 -0500

    Added a link_to props to the help button component.

--- source/components/help-button.js
+++ source/components/help-button.js
@@ -6,7 +6,7 @@ import Icon from './icon'
 class HelpButton extends React.Component {
 	render() {
 		return (
-			<RoundButton><Link to='/help'><Icon name='ionicon-help'/></Link></RoundButton>
+			<RoundButton><Link to={this.props.link_to}><Icon name='ionicon-help'/></Link></RoundButton>
 		)
 	}
 }
