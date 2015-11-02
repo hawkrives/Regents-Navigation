1f2df3fd54d73235a9c613b32978151e64d7d107
knightflight377 <emholm12@gmail.com>
2015-11-01 22:00:32 -0600

    Changed file path for styles. Deleted volume setting. Started trying to use text size button.

--- source/components/settings.js
+++ source/components/settings.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import SettingsButton from './settings-button'
+import TextSizeButton from './text-size-button'
 import MainMenuButton from './main-menu-button'
 import HelpButton from './help-button'
 import Icon from './icon'
-import styles from './settings.less'
+import styles from './our-styles.less'
 
 class Settings extends React.Component {
 	render() {
@@ -11,19 +12,12 @@ class Settings extends React.Component {
 			<div>
 				<h1 className= {styles['settings']}>Settings</h1>
 				<div>
-					<h3 className= {styles['settings-type']}>Volume</h3>
-					<SettingsButton>On</SettingsButton><SettingsButton>Off</SettingsButton>
-				</div> <br/>
-				<div>
-					<input type='range' min='0' max='100' value='50' />
-				</div>
-				<div>
 					<h3 className= {styles['settings-type']}>Color Scheme</h3>
 					<SettingsButton>Default</SettingsButton><SettingsButton>Red/Green</SettingsButton><SettingsButton>Custom</SettingsButton>
 				</div>
 				<div>
 					<h3 className= {styles['settings-type']}>Text Size</h3>
-					<SettingsButton><Icon name='ionicon-minus'/></SettingsButton><SettingsButton>Default</SettingsButton><SettingsButton><Icon name='ionicon-plus'/></SettingsButton>
+					<TextSizeButton type = 'subtract'><Icon name='ionicon-minus'/></TextSizeButton><SettingsButton>Default</SettingsButton><SettingsButton><Icon name='ionicon-plus'/></SettingsButton>
 				</div>
 				<div>
 					<MainMenuButton/> <br/>
