1b86a08fbb640f253814465802e40e45df796983
knightflight377 <emholm12@gmail.com>
2015-11-01 21:59:38 -0600

    Changed file path for styles.

--- source/components/settings-help.js
+++ source/components/settings-help.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './settings-help.less'
+import styles from './our-styles.less'
 import BackButton from './back-button'
 
 class SettingsHelp extends React.Component {
