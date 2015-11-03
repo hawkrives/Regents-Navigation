15c99d47522ef18d2510eacf8b3d63edaa30289e
knightflight377 <emholm12@gmail.com>
2015-11-02 20:45:15 -0600

    Changed the type of text.

--- source/components/settings.js
+++ source/components/settings.js
@@ -11,12 +11,12 @@ class Settings extends React.Component {
 			<div>
 				<h1 className= {styles['settings']}>Settings</h1>
 				<div>
-					<h3 className= {styles['settings-type']}>Color Scheme</h3>
+					<h2 className= {styles['settings-type']}>Color Scheme</h2>
 					<SettingsButton>Default</SettingsButton><SettingsButton>Red/Green</SettingsButton><SettingsButton>Custom</SettingsButton>
 				</div>
 				<div>
-					<h3 className= {styles['settings-type']}>Text Size</h3>
-					<h4 className= {styles['text-size-type']}>To adjust the size of the text, please do one of the following:</h4>
+					<h2 className= {styles['settings-type']}>Text Size</h2>
+					<p className= {styles['text-size-type']}>To adjust the size of the text, please do one of the following:</p>
 					<p className= {styles['text-size-type']}>On a Mac, hold <button>Command </button> while pressing either <button><Icon name='ionicon-plus'/></button> or <button><Icon name='ionicon-minus'/></button>.</p>
 					<p className= {styles['text-size-type']}>On a Windows, hold <button>Control </button> while pressing either <button><Icon name='ionicon-plus'/></button> or <button><Icon name='ionicon-minus'/></button>.</p>
 				</div>
