74b45c6e4ae07f8b390e88889b8c22e1aa3a1f90
knightflight377 <emholm12@gmail.com>
2015-12-10 20:30:37 -0600

    The Search and Locations button now link to the correct pages.

--- source/components/menu-bar.js
+++ source/components/menu-bar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 import MainNavigationButton from './main-navigation-button'
 import styles from './our-styles.less'
 
@@ -6,10 +7,10 @@ class MenuBar extends React.Component {
 	render() {
 		return (
 			<div>
-				<MainNavigationButton>Search</MainNavigationButton>
+				<MainNavigationButton><Link to='/search'>Search</Link></MainNavigationButton>
 				<MainNavigationButton>Save</MainNavigationButton>
 				<p className={styles['destination']}>Destination</p>
-				<MainNavigationButton>Locations</MainNavigationButton>
+				<MainNavigationButton><Link to='commondestinations'>Locations</Link></MainNavigationButton>
 				<MainNavigationButton>Labels</MainNavigationButton> <hr/>
 			</div>
 			)
