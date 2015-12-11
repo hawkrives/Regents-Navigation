03b6778f7b77deed48942bce0ebb9fdd1831e228
Hawken Rives <hawkrives@gmail.com>
2015-12-11 13:10:58 -0600

    remove react-imagemap

--- source/components/floor-map.js
+++ source/components/floor-map.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import ImageLoader from 'react-imageloader'
 
-/* Displays the 2D floor map. 
+/* Displays the 2D floor map.
    Currently not implemented */
 class FloorMap extends React.Component {
 	render() {
-		return(
-			<ImageLoader
-				src='/images/map_floor_2.png'
-				wrapper={React.DOM.div} />
-			)
+		return (
+			<img src='/images/map_floor_2.png' />
+		)
 	}
 }
 
